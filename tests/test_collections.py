c3119681da2bee29acf0e1fb5f996401d308df64
Nick Ludwig <nick.ludwig223@gmail.com>
2022-12-09 16:32:57 -0600

    added more tests to test_collections

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -1,6 +1,8 @@
 import os
 from typing import Any, List, Tuple
 
+import pytest
+
 from purviewautomation import PurviewCollections, ServicePrincipalAuthentication
 
 TENANT_ID = os.environ["purviewautomation-tenant-id"]
@@ -72,17 +74,23 @@ def test_create_collections_multiple02():
         assert name in friendly_names
         assert name2 in friendly_names
 
+def test_create_collections_type_error():
+    with pytest.raises(ValueError):
+        collections = {"name1": "name2"}
+        client.create_collections(start_collection="My-Company", collection_names=collections)
+
 
 # Delete collections
 def test_delete_collection():
     client.delete_collections("mytest1")
-    client.delete_collections("my test 2")
+    client.delete_collections("my test 2", safe_delete="client")
+    
 
 
 def test_delete_collections_recursively():
-    client.delete_collections_recursively("My-Company")
-    real_name = client.get_real_collection_name("My-Company")
-    child_collections = client.get_child_collection_names(real_name)
+    client.delete_collections_recursively("My-Company", safe_delete="client")
+    my_company_real_name = client.get_real_collection_name("My-Company")
+    child_collections = client.get_child_collection_names(my_company_real_name)
     assert child_collections["count"] == 0
 
 
@@ -92,3 +100,25 @@ def test_delete_collections_recursively02():
     collections = client.list_collections(only_names=True)
     friendly_names = [coll["friendlyName"] for coll in collections.values()]
     assert "My-Company" not in friendly_names
+
+
+# Get real collection name
+def test_get_real_collection_name_error():
+    with pytest.raises(ValueError):
+        client.get_real_collection_name(collection_name="fake collection")
+
+
+def test_force_actual_name():
+    client.create_collections(start_collection=PURVIEW_ACCOUNT_NAME, collection_names=["Collection 1", "Collection 2"])
+    # create duplicate collection name
+    client.create_collections("Collection 1", "duplicatename")
+    client.create_collections("Collection 2", "duplicatename")
+    client.create_collections(start_collection="duplicatename", collection_names="test sub 1", force_actual_name=True)
+    name, friendly_names = collection_check_helper("test sub 1")
+    assert name[0] in friendly_names
+
+def test_multiple_friendly_names():
+    with pytest.raises(ValueError):
+        client.create_collections(start_collection="duplicatename", collection_names="test2")
+
+
