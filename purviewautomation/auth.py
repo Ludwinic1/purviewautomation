0401833b6abd09324d031ad08b3de73e9dd4eae2
Nick Ludwig <nick.ludwig223@gmail.com>
2023-01-25 20:37:57 -0600

    updated auth with msal

--- purviewautomation/auth.py
+++ purviewautomation/auth.py
@@ -1,32 +1,36 @@
 from datetime import datetime
 
-import requests
+from msal import ConfidentialClientApplication
+
+
+# Base authentication
+def _msal_get_access_token(tenant_id, client_id, client_secret, scope):
+    app = ConfidentialClientApplication(
+        client_id=client_id, client_credential=client_secret, authority=f"https://login.microsoftonline.com/{tenant_id}"
+    )
+    result = None
+    result = app.acquire_token_silent(scopes=scope, account=None)
+
+    if not result:
+        result = app.acquire_token_for_client(scopes=scope)
+
+    if "access_token" in result:
+        return result["access_token"]
+    else:
+        raise ValueError(result.get("error") + ": " + result.get("error_description"))
 
 
 class ServicePrincipalAuthentication:
-    def __init__(self, tenant_id: str, client_id: str, client_secret: str):
-        self.url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
-        self.data = {
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "grant_type": "client_credentials",
-            "resource": "https://purview.azure.net",
-        }
-        self.access_token = None
-        self.access_token_expiration = datetime.now()
+    def __init__(self, tenant_id, client_id, client_secret):
+        self.tenant_id = tenant_id
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-    def _set_access_token(self):
-        access_token_request = requests.post(url=self.url, data=self.data)
-        if access_token_request.status_code != 200:
-            access_token_request.raise_for_status()
-
-        self.access_token = access_token_request.json()["access_token"]
-        self.access_token_expiration = datetime.fromtimestamp(int(access_token_request.json()["expires_on"]))
-
-    def get_access_token(self):
-        if self.access_token_expiration <= datetime.now():
-            self._set_access_token()
-        return self.access_token
+    def get_access_token(self, scope="https://purview.azure.net/.default"):
+        access_token = _msal_get_access_token(
+            tenant_id=self.tenant_id, client_id=self.client_id, client_secret=self.client_secret, scope=[scope]
+        )
+        return access_token
 
 
 class AzIdentityAuthentication:
