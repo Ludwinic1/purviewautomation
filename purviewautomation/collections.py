cd8104f072eaa23b89b7ca377b52fac04127ffa6
danward <dan.ward@mesh-ai.com>
2023-06-07 11:29:27 +0100

    If FriendlyName is a valid collectionName, but it is already in use, will now force generating a unique identifier.

--- purviewautomation/collections.py
+++ purviewautomation/collections.py
@@ -184,16 +184,19 @@ class PurviewCollections:
         ]
         return friendly_names
 
-    def _verify_collection_name(self, collection_name: str) -> str:
+    def _verify_collection_name(self, collection_name: str, uniqueness_retries: Optional[int] = 5) -> str:
         """Checks if the collection_name meets the Purview naming requirements.
 
         Args:
             collection_name: Name to check.
+            uniqueness_retries: The number of attempts to make for creating a unique name.
+                                Unlikely to happen even once, but exists for handling edgecases.
 
         Returns:
             The original collection_name or a random six character
                 lowercase string if requirements are not met.
         """
+        print(collection_name)
         pattern = "[a-zA-Z0-9]+"
         collection_check_pattern = re.search(pattern, collection_name)
         if collection_check_pattern:
@@ -203,6 +206,16 @@ class PurviewCollections:
                 collection_name = "".join(random.choices(string.ascii_lowercase, k=6))
         else:
             collection_name = "".join(random.choices(string.ascii_lowercase, k=6))
+        # Check if collection_name already in use, generate new if so
+        i = 0
+        existing_collection_names = self.list_collections(only_names=True).keys()
+        while collection_name in existing_collection_names and i < uniqueness_retries:
+            i += 1
+            collection_name = "".join(random.choices(string.ascii_lowercase, k=6))
+        if i == uniqueness_retries:
+            raise SystemError(f"Did not create unique name within {uniqueness_retries} attempts. Aborting.")
+        print(collection_name)
+        print("===")
         return collection_name
 
     def _return_updated_collection_name(
